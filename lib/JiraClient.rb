98aa56e754ef4d16662d1ebaf9b24b0786eb54b1
Callum Anderson <callum.anderson@mendeley.com>
2013-06-21 12:16:00 +0100

    Adding sll

--- lib/JiraClient.rb
+++ lib/JiraClient.rb
@@ -1,11 +1,16 @@
 require_relative 'lib_require'
 require 'rubygems'
 require 'httparty'
+require 'json'
+require 'openssl'
 
 class JiraClient
   attr_accessor :auth
   attr_accessor :req_path
   attr_accessor :host
+  attr_accessor :port
+
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
   # Constructor for the JiraClient
   # @param [String] username - the connecting username
@@ -24,23 +29,21 @@ class JiraClient
   # Connects to Jira with basic authentication
   # @return [String] resp -  the http response from connection
   def connect
-    req = URI.escape(@req_path)
-    p req
-    begin
-      p "attempting to connect"
-    @resp = HTTParty.get(
-        "#{@host}#{req}",
-        :basic_auth => @auth
-    )
-        p "done connecting"
-    rescue => e
-      p "entering fail"
-      @resp = e.response
+
+      url = URI.encode(@host + @req_path)
+      uri = URI.parse(url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request = Net::HTTP::Get.new(uri.request_uri)
+      request.basic_auth 'username', 'password'
+      request["Content-Type"] = "application/json"
+
+      @response = JSON.parse(http.request(request).body)
     end
-    end
+  end
 
   def get_response
     connect
-    @resp.code.to_s
-  end
+    return @response
   end
